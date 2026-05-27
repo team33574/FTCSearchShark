2190831ae8ade01d11a41b5c6340f510d7c65847
team33574 <team33574@penguincodingschool.com>
2026-05-27 15:32:19 -0400

    Add files via upload

---
+++ header.js
@@ -0,0 +1,16 @@
+document.write(`
+<header class="topbar">
+
+  <a href="index.html" class="logo-link">
+    <div class="logo-circle">
+      <img src="SearchSharkLogo.png" class="logo" alt="Logo">
+    </div>
+  </a>
+
+  <nav class="nav">
+    <a href="decode.html">Decode Rule Search</a>
+    <a href="biobuzz.html">BioBuzz Rule Search</a>
+  </nav>
+
+</header>
+`);
