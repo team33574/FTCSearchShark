b5732481d8f05c15b1995b4ebccc61ea77dd0642
team33574 <team33574@penguincodingschool.com>
2026-05-27 18:58:49 -0400

    Update header.js

--- header.js
+++ header.js
@@ -1,6 +1,7 @@
-document.write(`
-<header class="topbar">
+const header = document.createElement("header");
+header.className = "topbar";
 
+header.innerHTML = `
   <a href="index.html" class="logo-link">
     <div class="logo-circle">
       <img src="SearchSharkLogo.png" class="logo" alt="Logo">
@@ -11,6 +12,8 @@ document.write(`
     <a href="decode.html">Decode Rule Search</a>
     <a href="biobuzz.html">BioBuzz Rule Search</a>
   </nav>
+`;
 
-</header>
-`);
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.prepend(header);
+});
